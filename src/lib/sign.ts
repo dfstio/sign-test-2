a662847197967339addc4049dfd55697a7fd5844
DFST <mk@dfst.io>
2024-05-26 19:35:44 +0300

    simple

--- src/lib/sign.ts
+++ src/lib/sign.ts
@@ -1,6 +1,8 @@
-import { contractCloud } from "./cloud";
+//import { contractCloud } from "./cloud";
+import { contract } from "./contract";
 
 export async function sign(value: number) {
   console.log(value);
-  return await contractCloud(value);
+  //return await contractCloud(value);
+  return await contract(value);
 }
